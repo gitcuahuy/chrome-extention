9ca6b87982e9ed366dfd9dc1e6b265ef58a9fc78
My Name <huy.nguyenquang@evotek.vn>
2023-10-08 13:20:37 +0700

    xxx

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+const ROOT_ELEMENT_TAG = 'app-root';
+
+let rootElement = document.querySelector(ROOT_ELEMENT_TAG);
+
+if (!rootElement) {
+  rootElement = document.createElement(ROOT_ELEMENT_TAG);
+  rootElement.id = 'angular-chrome-app';
+  document.body.appendChild(document.createElement(ROOT_ELEMENT_TAG));
+}
+
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
